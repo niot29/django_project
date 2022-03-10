8888584e4d6dc8e12d8df75e2825e38264df9dc4
Nils Ottosson <ottossonnils@hotmail.com>
2022-03-10 16:35:59 +0100

    Add Post db and registr in admin.py

--- blog/views.py
+++ blog/views.py
@@ -2,28 +2,13 @@ from multiprocessing import context
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import views
-
-posts = [
-    {
-        'author': 'CoryMs',
-        'title': 'Blog Post1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2019'
-    },
-      {
-        'author': 'CoryMs2',
-        'title': 'Blog Post2',
-        'content': 'Second post content',
-        'date_posted': 'October 27, 2020'
-    }
-]
-
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts,
-        'title': 'killer'
+       'posts': Post.objects.all()
+      
     }
     return render(request,'blog/home.html',context)
 
