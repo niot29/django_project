b3aa2a7af3c8605ce29f8ed8a7216b9e02e15538
Nils Ottosson <ottossonnils@hotmail.com>
2022-03-17 00:14:14 +0100

    Part-8 Done: add user profile and image. extra add customer field in model uid & desc

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth import views as auth_views
+from django.conf import Settings, settings
+from django.conf.urls.static import static
+from tomlkit import document
 from users import views as user_views
 
 import blog
@@ -31,3 +34,6 @@ urlpatterns = [
     path('', include('blog.urls')),
     
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- users/models.py
+++ users/models.py
@@ -1,10 +1,14 @@
+import uuid
 from email.policy import default
 from django.db import models
 from django.contrib.auth.models import User
+from setuptools import Require
 
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    desc = models.TextField(max_length=250,blank=True)
+    uid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     
     def __str__(self) -> str:
         return f'{self.user.username} Profile'
