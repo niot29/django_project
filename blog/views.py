1b5ce774d1dcadeb4a4922826ec38dc85dfcf512
Nils Ottosson <ottossonnils@hotmail.com>
2022-03-17 16:17:02 +0100

    Part 10. time: 10:37 - Add class view on Blog list and Detail calss view on blog

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from multiprocessing import context
+from re import template
 from django.shortcuts import render
-from django.http import HttpResponse
-from . import views
+from django.views.generic import ListView,DetailView
 from .models import Post
 
 
@@ -11,6 +11,18 @@ def home(request):
       
     }
     return render(request,'blog/home.html',context)
+
+
+class PostListView(ListView):
+      model = Post
+      template_name = 'blog/home.html' #default path to view: <app>/<model>_<viewtype>.html  blog/post_list.html
+      context_object_name = 'posts'
+      ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+      model = Post
+      
 
 def about(requst):
    return render(requst,'blog/about.html',{'title': 'About'})
