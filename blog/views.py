40f7a80500b2a311a0e7fdc841305f0b32c7bf27
stniot1 <stniot1@d11-stniot1.vdc.saab.se>
2022-03-22 13:30:34 +0100

    Part-11 done: url filter blog by user

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from mmap import PAGESIZE
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, get_list_or_404
+from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
 from django.views.generic import (
       ListView,
@@ -26,6 +26,19 @@ class PostListView(ListView):
       ordering = ['-date_posted']
       paginate_by = 5
 
+
+
+# = /home with user filter
+class UserPostListView(ListView):
+      model = Post
+      template_name = 'blog/user_posts.html' #default path to view: <app>/<model>_<viewtype>.html  blog/post_list.html
+      context_object_name = 'posts'
+      paginate_by = 5
+
+      def get_queryset(self):
+            user = get_object_or_404(User,username=self.kwargs.get('username'))
+            return Post.objects.filter(author=user).order_by('-date_posted')
+      
 class PostDetailView(DetailView):
       model = Post
 
