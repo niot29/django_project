dff85bb440138fdff61783a203c4d6d26c28e168
stniot1 <stniot1@d11-stniot1.vdc.saab.se>
2022-03-22 15:28:17 +0100

    Part-11: Not workning as it shuold.

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+from pickle import TRUE
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,3 +137,12 @@ LOGIN_REDIRECT_URL = 'blog-home'
 
 # Redirect if not login
 LOGIN_URL = 'login'
+
+
+## SMTP setting for Gmail
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = ' smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = TRUE
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
