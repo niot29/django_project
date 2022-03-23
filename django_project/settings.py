403411cd62b3b0a55c8ec6ff687652c45da5f930
stniot1 <stniot1@d11-stniot1.vdc.saab.se>
2022-03-23 13:43:02 +0100

    part-11: done by cant send mail från gmail

--- django_project/settings.py
+++ django_project/settings.py
@@ -141,8 +141,8 @@ LOGIN_URL = 'login'
 
 ## SMTP setting for Gmail
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = ' smtp.gmail.com'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_USE_TLS = TRUE
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'nisse.ottosson@gmail.com'
+EMAIL_HOST_PASSWORD = 'Return2Basic'
